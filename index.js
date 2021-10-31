fd8cb73c0e5df3554aa77df69e17addd6be03e71
Nazmus Sakib <sakibkhancse09@gmail.com>
2021-10-31 21:20:57 +0600

    Add middleware

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+  }) 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o7umd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
